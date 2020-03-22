2fd15bb750e129de00169716039fe02bf2c54d45
cuboktahedron <cuboktahedron@gmail.com>
2020-03-22 10:33:14 +0900

    introduce jest

---
+++ src/test/actions/lib/Color.ts
@@ -0,0 +1,9 @@
+import Color from "../../../actions/lib/Color";
+
+describe("Color", () => {
+  describe("Constructor", () => {
+    it("should make object with specified color", () => {
+      expect(new Color("123456").rgba).toBe("123456ff");
+    });
+  });
+});
