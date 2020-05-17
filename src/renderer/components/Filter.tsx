22336f953d691ec57ebd218983ec65aa17d8daf7
cuboktahedron <cuboktahedron@gmail.com>
2020-05-17 10:25:25 +0900

    Ellipsis filter params

--- src/renderer/components/Filter.tsx
+++ src/renderer/components/Filter.tsx
@@ -1,14 +1,18 @@
 import {
+  createMuiTheme,
   FormControl,
   IconButton,
   ListItem,
-  NativeSelect
+  MuiThemeProvider,
+  NativeSelect,
+  TextField,
+  Tooltip
 } from "@material-ui/core";
 import DeleteIcon from "@material-ui/icons/Delete";
 import SettingsIcon from "@material-ui/icons/Settings";
 import { changeFilter } from "actions/filterAction";
 import { deleteFilter } from "actions/layerAction";
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { LFT_ID } from "stores/filter/id";
 import { LFT_OPACITY } from "stores/filter/opacity";
@@ -19,9 +23,29 @@ type FilterProps = FilterState & {
   layerId: number;
 };
 
+const theme = createMuiTheme({
+  overrides: {
+    MuiTooltip: {
+      tooltip: {
+        fontSize: "1em"
+      }
+    },
+    MuiInputBase: {
+      input: {
+        overflow: "hidden",
+        textOverflow: "ellipsis"
+      }
+    }
+  }
+});
+
 const Filter: React.FC<FilterProps> = props => {
   const dispatch = useDispatch();
   const [settingsOpened, setSettingsOpened] = React.useState(false);
+  const [disableParamsToolTip, setDisableParamsToolTip] = useState<boolean>(
+    true
+  );
+  let paramsRef: HTMLInputElement | null = null;
 
   const changeTypeHandler = (e: React.ChangeEvent<HTMLSelectElement>): void => {
     const value = e.currentTarget.value as FilterType;
@@ -55,8 +79,17 @@ const Filter: React.FC<FilterProps> = props => {
     dispatch(deleteFilter(props.layerId, props.id));
   };
 
+  const handleParamsMouseEnter = (): void => {
+    if (paramsRef === null) {
+      setDisableParamsToolTip(false);
+      return;
+    }
+
+    setDisableParamsToolTip(paramsRef.scrollWidth <= paramsRef.clientWidth);
+  };
+
   return (
-    <div>
+    <MuiThemeProvider theme={theme}>
       <FilterProperty
         opened={settingsOpened}
         layerId={props.layerId}
@@ -75,6 +108,24 @@ const Filter: React.FC<FilterProps> = props => {
             <option value={LFT_OPACITY}>opacity</option>
           </NativeSelect>
         </FormControl>
+        <Tooltip
+          disableHoverListener={disableParamsToolTip}
+          title={JSON.stringify(props.property)}
+          style={{ fontSize: "2rem", color: "red" }}
+        >
+          <div style={{ marginLeft: "8px" }}>
+            <TextField
+              onMouseEnter={handleParamsMouseEnter}
+              inputRef={(ref): void => {
+                paramsRef = ref;
+              }}
+              InputProps={{
+                readOnly: true
+              }}
+              value={props.property}
+            ></TextField>
+          </div>
+        </Tooltip>
         <IconButton onClick={openSettingsHandler}>
           <SettingsIcon />
         </IconButton>
@@ -82,7 +133,7 @@ const Filter: React.FC<FilterProps> = props => {
           <DeleteIcon />
         </IconButton>
       </ListItem>
-    </div>
+    </MuiThemeProvider>
   );
 };
 
