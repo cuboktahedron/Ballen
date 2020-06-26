48bc592f582dcc781c025e2a3cbeb3cc5a0724f4
cuboktahedron <cuboktahedron@gmail.com>
2020-06-27 08:54:18 +0900

    Don't use redux-logger in production

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,8 +1,13 @@
 import rootReducer from "reducers/rootReducer";
 import { applyMiddleware, createStore } from "redux";
-import logger from "redux-logger";
 import thunk from "redux-thunk";
 
-const middlewares = [thunk, logger];
+const middlewares = [thunk];
+
+if (process.env.NODE_ENV !== "production") {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const { logger } = require("redux-logger");
+  middlewares.push(logger);
+}
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
